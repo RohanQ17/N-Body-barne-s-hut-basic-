82ac922420c890c0f149a234ab8caaec9cd5934e
RohanQ17 <axelb4467@gmail.com>
2025-11-08 13:45:05 +0530

    starting boilerplate code, cmake config and supposed directories

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <iostream>
+#include <vector>
+#include <random>
+
+int main() {
+    return 0;
+}
